a35d847b47f838b74f604495cdd5976d9f11b4de
ArdaTekinRat <arda.tkn120634@gmail.com>
2023-09-24 18:34:30 +0300

    proje

---
+++ Models/Classes.cs
@@ -0,0 +1,12 @@
+﻿namespace studentschool.Models
+{
+    public class Classes
+    {
+        public int Id { get; set; }
+        public string SinifAdi { get; set; }
+        public virtual List<Student> Students { get; set; }
+     
+
+
+    }
+}
